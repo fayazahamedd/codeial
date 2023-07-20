7945b32a0a87e2cb5d9a30ed0b1139ab5b33f3ed
Fayaz Ahamed D <fayazahamed.d@dataparq.com>
2023-07-20 16:03:39 +0530

    First Commit: Creating the directory

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Some error in app.listen");
+    return;
+  } else {
+    console.log(`Server is running on port ${port}`);
+  }
+});
