6692a04b39de35aa938a655d527fd070dd8a5ec4
Fayaz Ahamed D <fayazahamed.d@dataparq.com>
2023-07-26 21:26:39 +0530

    Auth for post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,23 @@
-module.exports.home = function (req, res) {
-  console.log(req.cookies)
-  // res.cookie = ('user_id',25)
-  return res.render('home', {
-    title: "Home"
-  });
-};
+const Post = require("../models/post");
 
+module.exports.home = async function (req, res) {
+  try {
+    const posts = await Post.find({}).populate('user','name').exec();
+    // console.log(posts)
+    console.log('**************',req.user)
+
+    console.log('posts',posts);
+    return res.render('home', {
+      title: "Home ~ post",
+      user: {
+        name: req?.user?.name,
+        email: req?.user?.email,
+        id: req?.user?._id,
+      },
+      posts: posts
+    });
+  } catch (err) {
+    console.log("Error fetching posts:", err);
+    return;
+  }
+};
